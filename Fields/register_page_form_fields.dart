2f42555726f3ce381e3c5e12589c9c3bc9945893
Kartal Karabulut <95976872+kartalkarabulut@users.noreply.github.com>
2023-11-16 21:14:31 +0300

    Add files via upload

---
+++ Fields/register_page_form_fields.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:task_manager/screens/home/home_screen.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  TextEditingController _emailController = TextEditingController();
+  FocusNode _emailFocus = FocusNode();
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _nameController = TextEditingController();
+  FocusNode _nameFocus = FocusNode();
+  TextEditingController _passwordController = TextEditingController();
+  FocusNode _passwordFocus = FocusNode();
+  TextEditingController _surnameController = TextEditingController();
+  FocusNode _surnameFocus = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //backgroundColor: Color(0xFFB8E1DD),
+      appBar: AppBar(
+        title: const Text("Register Page"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: _nameController,
+                  focusNode: _nameFocus,
+                  decoration: InputDecoration(labelText: 'Ad'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Name cant be empty";
+                    }
+                    return null;
+                  },
+                  onEditingComplete: () {
+                    _fieldFocusChange(context, _nameFocus, _surnameFocus);
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: _surnameController,
+                  focusNode: _surnameFocus,
+                  decoration: InputDecoration(labelText: 'Surname'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Surname cant be empty';
+                    }
+                    return null;
+                  },
+                  onEditingComplete: () {
+                    _fieldFocusChange(context, _surnameFocus, _emailFocus);
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: _emailController,
+                  focusNode: _emailFocus,
+                  decoration: InputDecoration(labelText: 'E-mail'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'E-mail cant be empty';
+                    } else if (!value.contains('@')) {
+                      return 'Enter a valid email';
+                    }
+                    return null;
+                  },
+                  onEditingComplete: () {
+                    _fieldFocusChange(context, _emailFocus, _passwordFocus);
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  focusNode: _passwordFocus,
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: 'Password'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Password cant be empty';
+                    }
+                    return null;
+                  },
+                  //onEditingComplete: register,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => HomeScreen()));
+                    }
+                  },
+                  child: const Text('Register'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+register(BuildContext context) {
+  Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) => HomeScreen(),
+    ),
+  );
+}
+
+void _fieldFocusChange(
+  BuildContext context,
+  FocusNode currentFocus,
+  FocusNode nextFocus,
+) {
+  currentFocus.unfocus();
+  FocusScope.of(context).requestFocus(nextFocus);
+}
